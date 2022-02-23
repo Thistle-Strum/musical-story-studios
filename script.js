ac792a51468000a6c5f2bb0a1b0cbc2cc461845a
david <lostmorgan@gmail.com>
2022-02-23 13:26:03 -0500

    dropdown menu at 700px

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ const menuButton = document.querySelector('.menuButton')
 let menuOpen = false;
 menuButton.addEventListener('click', function() {
     menuButton.classList.toggle('open');
+    console.log("jello")
 });
+
